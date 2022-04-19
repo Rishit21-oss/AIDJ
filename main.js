6d8f8e7fba8cb05f3ebf1e205a597cf6a3103c9c
Rishit21-oss <81700717+Rishit21-oss@users.noreply.github.com>
2022-04-19 20:37:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
 song ="";
+leftWristX="";
+leftWristY="";
+rightWristX="";
+rightWristY="";
 
 function setup() {
     canvas= createCanvas(600, 500);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose' , gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is Initialized");
+}
+
+function gotPoses(results){
+
+    if(results.length > 0){
+
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
 }
 
 function preload(){
@@ -17,4 +45,6 @@ function draw(){
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
